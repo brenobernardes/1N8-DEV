6b633d7b2e4cf1942b35b9e568a26bee4aa75236
Breno Bernardes <breno.pbernardes@gmail.com>
2022-04-29 18:55:14 -0300

    first commit

---
+++ src/pages/Home/home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './home.css';
+
+export default function Home () {
+    return (
+        <header>
+            <div className="header">
+                <ul>
+                    <li>Cadastro</li>
+                    <li>Lista</li>
+                    <li>Sobre mim</li>
+                </ul>
+            </div>
+
+            <div>
+
+            </div>
+        </header>
+    );
+}
