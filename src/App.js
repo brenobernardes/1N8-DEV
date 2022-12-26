353157ed5b1e74c08ef6bfb98f18fc8a7a1eb6c0
brenobernardes <breno.pbernardes@gmail.com>
2022-12-26 10:25:54 -0300

    update package.json

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./pages/Header/header";
 
--- src/pages/Footer/footer.js
+++ src/pages/Footer/footer.js
@@ -1,4 +1,5 @@
-import "./footer.css"
+import React from "react";
+import "./footer.css";
 
 export default function Footer () {
     return (
